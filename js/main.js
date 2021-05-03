00f942e8a54c9aa8c666c5acfce73628bdc1c4d0
Anup Krishna Kumar <akumar11@mt.iitr.ac.in>
2021-05-03 09:10:53 +0530

    first commit

---
+++ js/main.js
@@ -0,0 +1,40 @@
+const daysText = document.querySelector("#days");
+const hoursText = document.querySelector("#hours");
+const minutesText = document.querySelector("#minutes");
+const secondsText = document.querySelector("#seconds");
+let countdown = {
+  days: 13,
+  hours: 23,
+  minutes: 59,
+  seconds: 59,
+};
+
+updateCountdown();
+setInterval(updateCountdown, 1000);
+
+function updateCountdown() {
+  if (countdown.seconds > 0) {
+    countdown.seconds--;
+  } else {
+    countdown.seconds = 59;
+    countdown.minutes--;
+  }
+
+  if (countdown.minutes < 0) {
+    countdown.seconds = 59;
+    countdown.minutes = 59;
+    countdown.hours--;
+  }
+
+  if (countdown.hours === 0) {
+    countdown.seconds = 59;
+    countdown.minutes = 59;
+    countdown.hours = 23;
+    countdown.days--;
+  }
+
+  daysText.textContent = countdown.days;
+  hoursText.textContent = countdown.hours;
+  minutesText.textContent = countdown.minutes;
+  secondsText.textContent = countdown.seconds;
+}
